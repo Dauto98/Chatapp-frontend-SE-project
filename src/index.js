7f4f676c80f93afa62114263ae06301d1e03f260
Dauto98 <dauto98@gmail.com>
2019-04-01 22:37:24 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>hello haha</div>, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept();
+}
